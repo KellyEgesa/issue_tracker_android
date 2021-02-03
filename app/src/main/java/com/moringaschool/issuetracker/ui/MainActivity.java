d431645a6d7ed0a6a315d37190bc3aa69c7e5875
KellyEgesa <kelly.egesa@gmail.com>
2021-02-03 14:34:15 +0300

    worked on the drawer

--- app/src/main/java/com/moringaschool/issuetracker/ui/MainActivity.java
+++ app/src/main/java/com/moringaschool/issuetracker/ui/MainActivity.java
@@ -1,16 +1,39 @@
 package com.moringaschool.issuetracker.ui;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.os.Bundle;
+import android.view.View;
 
 import com.moringaschool.issuetracker.R;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
 public class MainActivity extends AppCompatActivity {
+    @BindView(R.id.drawer_layout)
+    DrawerLayout drawer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+    }
+
+    public void ClickMenu(View view){
+        openDrawer(drawer);
+    }
+
+    private static void openDrawer(DrawerLayout drawer) {
+        drawer.openDrawer(GravityCompat.START);
+    }
+
+    private static void closeDrawer(DrawerLayout drawer) {
+        if(drawer.isDrawerOpen(GravityCompat.START)){
+            drawer.closeDrawer(GravityCompat.START);
+        }
     }
 }
